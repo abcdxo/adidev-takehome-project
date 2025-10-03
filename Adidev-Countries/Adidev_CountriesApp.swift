1afb38e73953008f9591e4c5d7099fbac54ed0bd
Thinh Nguyen <nicknguyenios14@gmail.com>
2025-10-03 11:37:19 -0400

    Initial Commit

---
+++ Adidev-Countries/Adidev_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adidev_CountriesApp.swift
+//  Adidev-Countries
+//
+//  Created by Thinh Nguyen on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Adidev_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adidev-CountriesTests/Adidev_CountriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Adidev_CountriesTests.swift
+//  Adidev-CountriesTests
+//
+//  Created by Thinh Nguyen on 10/3/25.
+//
+
+import Testing
+@testable import Adidev_Countries
+
+struct Adidev_CountriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
